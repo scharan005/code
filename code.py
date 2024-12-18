960d64755b79a3944b7c30c4c40d026cb885d66e
Sri Charan Ponnada <115648527+scharan005@users.noreply.github.com>
2024-12-18 14:05:21 +0530

    Update code.py

--- code.py
+++ code.py
@@ -8,3 +8,6 @@ for line in '''$result'''.split('\n'):
 json.dump(old, open('$combined_json','w'), indent=2)
 "
 
+python -c "import json; old=json.load(open('$combined_json')); lines='''$result'''.split('\n'); 
+filtered=[json.loads(l.strip()) for l in lines if l.strip().startswith('{') and l.strip().endswith('}')]; 
+old.extend(filtered); json.dump(old, open('$combined_json','w'), indent=2)"
