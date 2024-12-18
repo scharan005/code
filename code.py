3cd2680cc98686e285703e751b88d2ed3a4aedd6
Sri Charan Ponnada <115648527+scharan005@users.noreply.github.com>
2024-12-18 13:55:45 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,9 +1,8 @@
-# Extract JSON lines and append them to combined_benchmarks.json
-      python -c "
+python -c "
 import json
 old = json.load(open('$combined_json'))
 for line in '''$result'''.split('\n'):
-    line=line.strip()
+    line = line.strip()
     if line.startswith('{') and line.endswith('}'):
         old.append(json.loads(line))
 json.dump(old, open('$combined_json','w'), indent=2)
